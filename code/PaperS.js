721b45b8cf831163100ed073e8cd9d2a7d0c4f21
TheKnightliestKnightofKnightlyKnights <colter.hulse@western.edu>
2017-05-04 10:46:14 -0600

    Add files via upload

---
+++ code/PaperS.js
@@ -0,0 +1,6 @@
+    function tokenGenerate() {
+      alert('IOt works');
+      var img = document.getElementById('tokenFile');
+    var raster = new Raster('img');
+    raster.position=view.center;
+}
--- code/jsCode.js
+++ code/jsCode.js
@@ -10,13 +10,32 @@ function send(){
   textBox.value= textBox.value + text;
 }
 
-function importmap(){
-  alert("Yay, it works!")
-}
+function selectMap(){
+  var imageLoader = document.getElementById('theFile');
+    imageLoader.addEventListener('change', handleImage, false);
+   var canvas = document.getElementById('canvas');
+   var ctx = canvas.getContext('2d');
 
-    function handle(e){
-        if(e.keyCode === 13){
-            e.preventDefault(); // Ensure it is only this code that rusn
+
+function handleImage(e){
+    var reader = new FileReader();
+    reader.onload = function(event){
+        var img = new Image();
+        img.onload = function(){
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img,0,0);
+        }
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);
+    }
+    }
+
+
+function handle(e){
+    if(e.keyCode === 13){
+        e.preventDefault(); // Ensure it is only this code that rusn
 
   var textBox=document.getElementById("textArea");
   var text = document.getElementById("tBox").value ;
@@ -24,6 +43,40 @@ function importmap(){
   document.getElementById("tBox").value="";
         }
     }
+
+function diceRoll20(){
+  var profi = parseFloat(document.getElementById("prof").value);
+  var attrBonus = parseFloat(document.getElementById("attriBonus").value);
+  var textBox=document.getElementById("textArea");
+  var base= Math.floor(Math.random()*20)+1;
+  var final = base+profi+attrBonus;
+  var text = "Base Roll: "+base+" Final Result: "+final;
+  textBox.value= textBox.value + text +"\n";
+}
+
+function diceRoll(){
+  var attrBonus = parseFloat(document.getElementById("attriBonus").value);
+  var numDice = parseFloat(document.getElementById("diceN").value);
+  var numSides = parseFloat(document.getElementById("diceS").value);
+  var finalDice = 0;
+  for(i=0;i<numDice;i++)
+  {
+    finalDice+=Math.floor(Math.random()*numSides)+1;
+  }
+  var final = finalDice+attrBonus;
+  var textBox=document.getElementById("textArea");
+  var text = ""+numDice+" d"+numSides+" = "+final;
+  textBox.value= textBox.value + text +"\n";
+}
+
+function performClick(elemId) {
+   var elem = document.getElementById(elemId);
+   if(elem && document.createEvent) {
+      var evt = document.createEvent("MouseEvents");
+      evt.initEvent("click", true, false);
+      elem.dispatchEvent(evt);
+   }
+}
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
@@ -33,4 +86,4 @@ window.onclick = function(e) {
         myDropdown.classList.remove('show');
       }
   }
-}
+};
